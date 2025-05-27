7a87b9ee7f11ef93f50beff5200a6573cf4f8695
SHIJU <118278088+shijuvtm@users.noreply.github.com>
2025-05-27 13:05:47 +0530

    Add files via upload

---
+++ ReactApp/src/components/dashboard/HealthStatusCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function HealthStatusCards() {
+  return (
+    <div className="health-cards">
+      <div className="card red">Lungs - 12 Apr 2021</div>
+      <div className="card yellow">Teeth - 19 Apr 2021</div>
+      <div className="card gray">Bone - 25 Apr 2021</div>
+    </div>
+  );
+}
